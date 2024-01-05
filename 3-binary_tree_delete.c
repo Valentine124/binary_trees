482a60979a90205e71e55284a4d7a0ebfc9f4e56
valentine124 <kaluvalentine60@gmail.com>
2024-01-04 17:37:42 -0800

    Task 3

---
+++ 3-binary_tree_delete.c
@@ -0,0 +1,19 @@
+#include "binary_trees.h"
+
+/**
+ * binary_tree_delete - Deletes an entire binary tree
+ * @tree: Pointer to the root of the tree
+ */
+void binary_tree_delete(binary_tree_t *tree)
+{
+	if (tree == NULL)
+		return;
+
+	if (tree->left != NULL)
+		binary_tree_delete(tree->left);
+	
+	if (tree->right != NULL)
+		binary_tree_delete(tree->right);
+
+	free(tree);
+}
